57208d38c5e4c3e0003403edca030672ab6d2e42
flohms18 <trouilletflorent@outlook.com>
2025-04-20 11:58:23 +0200

    Added API requests

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
+TMDB_API_KEY = '27beba95fd51654379e58b8e53c1c594'
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    query = request.args.get('query')
+    results = []
+    if query:
+        url = 'https://api.themoviedb.org/3/search/movie'
+        params = {'api_key': TMDB_API_KEY, 'query': query}
+        response = requests.get(url, params=params)
+        if response.ok:
+            data = response.json()
+            results = data.get('results', [])
+
+    return render_template('index.html',results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
